60a2df04d544772f61c04362e7541e1218d2f91d
anuradha trivedi <anuradha.trivedi@bakingo.com>
2019-07-19 14:27:38 +0530

    feedback-front-screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bulb from './image/bulb.png';
+import thumb1 from './image/thumb1.png';
+import thumb2 from'./image/thumb2.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  
+class App extends Component{
+ 
+  render(){
+    return(
+      <div className="container">
+      <div className="overlay-box"></div>
+      <div className="header">
+          <h4>Header</h4>
+           
+          <div className="tip-bar"> <img className="bulb" src={bulb} alt="bulb"></img>
+              <p>Tip</p>
+          </div>
+      </div>
+
+      <div>
+          <hr />
+      </div>
+      <div className="progress-bar">
+          <div className="improvement">
+
+          </div>
+
+      </div>
+
+
+
+      <div className="detail-box">
+
+          <div className="one">
+
+          <h3 className="heading">Hey Amit,<br/>How was your gift receiving<br/>experience?
+                </h3>
+
+              <p className="font" >Your feedback helps in improving the
+                  deliverey experience</p>
+
+
+              <div className="review-box">
+                  <div className="circle firstcircle">
+                      <img className="thumb" src={thumb1}></img>  
+                  </div>
+
+                  <div className="circle">
+                      <img className="thumb" src={thumb2}></img>
+                  </div>
+              </div>
+
+          </div>
+
+  </div>
+  <button className="submit-btn1">Submit & Continue</button>
+  </div>
+
+    )
+    
+  }
 }
+
+
 
 export default App;
